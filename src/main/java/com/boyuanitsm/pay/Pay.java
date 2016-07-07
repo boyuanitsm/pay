7b9898b0fd3379608615db44d1de7e4e8a35641f
hookszhang <hua.zhang@boyuanitsm.com>
2016-07-07 11:26:52 +0800

    Add QRCode lib

--- src/main/java/com/boyuanitsm/pay/Pay.java
+++ src/main/java/com/boyuanitsm/pay/Pay.java
@@ -1,10 +1,17 @@
 package com.boyuanitsm.pay;
 
+import net.glxn.qrgen.javase.QRCode;
+
+import java.io.*;
+
 /**
  * @author hookszhang on 7/7/16.
  */
 public class Pay {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
+        ByteArrayOutputStream stream = QRCode.from("Hello World").stream();
+        OutputStream outputStream = new FileOutputStream("/Users/zhanghua/Desktop/qrcode.png");
+        outputStream.write(stream.toByteArray());
     }
 }
