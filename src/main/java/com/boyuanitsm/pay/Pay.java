857308163418552e3e194430b8c0b98130807292
hookszhang <hua.zhang@boyuanitsm.com>
2016-07-07 10:14:16 +0800

    first commit

---
+++ src/main/java/com/boyuanitsm/pay/Pay.java
@@ -0,0 +1,10 @@
+package com.boyuanitsm.pay;
+
+/**
+ * @author hookszhang on 7/7/16.
+ */
+public class Pay {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
