27bab2c694c2e5aeb27215d231ba59de8d21a3ba
hookszhang <hua.zhang@boyuanitsm.com>
2016-07-15 10:36:58 +0800

    Add alipay test view

---
+++ src/test/java/com/boyuanitsm/pay/rest/AlipayResource.java
@@ -0,0 +1,18 @@
+package com.boyuanitsm.pay.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest controller Alipay Resource
+ *
+ * @author hookszhang on 7/15/16.
+ */
+@RestController
+@RequestMapping("api/alipay")
+public class AlipayResource {
+
+    private static Logger log = LoggerFactory.getLogger(AlipayResource.class);
+}
