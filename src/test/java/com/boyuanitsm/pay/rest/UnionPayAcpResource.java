a9ed4f6dd8ab77f0418f21b5e030c431f7151cc8
hookszhang <hua.zhang@boyuanitsm.com>
2016-07-27 14:31:16 +0800

    Update UnionPayAcpResource

--- src/test/java/com/boyuanitsm/pay/rest/UnionPayAcpResource.java
+++ src/test/java/com/boyuanitsm/pay/rest/UnionPayAcpResource.java
@@ -1,8 +1,11 @@
 package com.boyuanitsm.pay.rest;
 
 import com.boyuanitsm.pay.unionpay.acp.Acp;
+import com.boyuanitsm.pay.unionpay.acp.exception.SignValidateFailException;
 import com.boyuanitsm.pay.unionpay.acp.service.AcpService;
-import com.boyuanitsm.pay.unionpay.acp.service.token.OpenCardFront;
+import com.boyuanitsm.pay.unionpay.acp.service.ConsumeStatusQuery;
+import com.boyuanitsm.pay.unionpay.acp.service.token.*;
+import org.apache.commons.httpclient.HttpException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,12 @@ public class UnionPayAcpResource {
     private Logger log = LoggerFactory.getLogger(UnionPayAcpResource.class);
 
     private OpenCardFront openCardFront = new OpenCardFront();
+    private OpenQuery openQuery = new OpenQuery();
+    private ConsumeSMS consumeSMS = new ConsumeSMS();
+    private Consume consume = new Consume();
+    private OpenAndConsume openAndConsume = new OpenAndConsume();
+    private ConsumeStatusQuery consumeStatusQuery = new ConsumeStatusQuery();
+    private DeleteToken deleteToken = new DeleteToken();
 
     @RequestMapping("open_card_front")
     public String openCardFront(String orderId) {
@@ -53,5 +62,63 @@ public class UnionPayAcpResource {
             return new HashMap<>();
         }
         return allRequestMap;
+    }
+
+    @RequestMapping("open_query")
+    public Map<String, String> openQuery(String orderId, HttpServletRequest request) {
+        try {
+            return openQuery.query(orderId, null);
+        } catch (SignValidateFailException e) {
+            e.printStackTrace();
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("consume_sms")
+    public Map<String, String> consumeSMS(String orderId, String txnAmt, String phoneNo, String token) {
+        try {
+            return consumeSMS.request(orderId, txnAmt, phoneNo, token);
+        } catch (SignValidateFailException e) {
+            e.printStackTrace();
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("consume")
+    public Map<String, String> consume(String orderId, String txnAmt, String token, String smsCode, String reqReserved) {
+        try {
+            return consume.consume(orderId, txnAmt, token, smsCode, reqReserved);
+        } catch (HttpException e) {
+            e.printStackTrace();
+        } catch (SignValidateFailException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("open_and_consume")
+    public String openAndConsume(String orderId, String txnAmt, String accNo) {
+        return openAndConsume.build(orderId, txnAmt, accNo);
+    }
+
+    @RequestMapping("consume_status_query")
+    public Map<String, String> consumeStatusQuery(String orderId, String txnTime) {
+        try {
+            return consumeStatusQuery.query(orderId, txnTime);
+        } catch (SignValidateFailException e) {
+            e.printStackTrace();
+        } catch (HttpException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping("delete_token")
+    public Map<String, String> deleteToken() {
+        return null;
     }
 }
